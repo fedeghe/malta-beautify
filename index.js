dc9582fec7934c0d1a40416d625291d9cb836e55
fedeghe <fedeghe@gmail.com>
2019-07-06 22:49:19 +0200

    reject set

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
 require('malta').checkDeps('js-beautify');
 
 var beautify = require("js-beautify"),
-	path = require('path'),
-	fs = require('fs');
+    path = require('path'),
+    fs = require('fs');
 
 function malta_beautify(o, options) {
 
-	var self = this,
-		name = o.name,
-		start = new Date(),
-		msg,
-		ext = o.name.split('.').pop(),
-		proceed = ext in beautify,
-		pluginName = path.basename(path.dirname(__filename));
+    var self = this,
+        name = o.name,
+        start = new Date(),
+        msg,
+        ext = o.name.split('.').pop(),
+        proceed = ext in beautify,
+        pluginName = path.basename(path.dirname(__filename));
 
-	options = options || {};
+    options = options || {};
 
-	if (proceed){
-		try {
-			o.content = beautify[ext](o.content, options) + "";
-		} catch (err) {
-			self.doErr(err, o, pluginName);
-		}
-	}
-
-	return function (solve, reject){
-		if (proceed) {
-			fs.writeFile(o.name, o.content, function(err) {
-				err && self.doErr(err, o, pluginName);
-				msg = 'plugin ' + pluginName.white() + ' wrote ' + o.name + ' (' + self.getSize(o.name) + ')';
-				solve(o);
-				self.notifyAndUnlock(start, msg);
-			});
-		} else {
-			solve(o);
-			self.notifyAndUnlock(start, msg);
-		}
-	};
+    return function (solve, reject) {
+        if (proceed) {
+            try {
+                o.content = beautify[ext](o.content, options) + "";
+                fs.writeFile(o.name, o.content, function (err) {
+                    if (err) {
+                        self.doErr(err, o, pluginName);
+                    }
+                    msg = 'plugin ' + pluginName.white() + ' wrote ' + o.name + ' (' + self.getSize(o.name) + ')';
+                    err
+                        ? reject(`Plugin ${pluingName} write error:\n${err}`)
+                        : solve(o);
+                    self.notifyAndUnlock(start, msg);
+                });
+            } catch (err) {
+                reject(`Plugin ${pluingName} beautyfication error:\n${err}`)
+                self.doErr(err, o, pluginName);
+            }
+        } else {
+            solve(o);
+            self.notifyAndUnlock(start, msg);
+        }
+    };
 }
 malta_beautify.ext = ['js', 'css', 'html', 'pug', 'md', 'less', 'scss', 'ts', 'coffee', 'haml'];
 module.exports = malta_beautify;
